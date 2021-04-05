01359c2214fa1d84bd605962dc23ac562e66dfb9
LucianoCrocco <lucianocrocco@outlook.com>
2021-04-05 00:01:49 -0300

    6-3

--- [Martes] Laboratorio/[30-03] Clase 6/Ejercicio 6-3/src/main.c
+++ [Martes] Laboratorio/[30-03] Clase 6/Ejercicio 6-3/src/main.c
@@ -9,11 +9,87 @@
  *  Gomez, Juan Ignacio
  */
 
-#include <stdio.h>
+#include <stdio_ext.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+
+#define TAMANIO_STRING 40
+#define TAMANIO_STRING_DOS 80
+
+void strlwr(char string[], int lenght);
 
 int main (void){
+
+	int lenght;
+	int i;
+
+	char nombre[TAMANIO_STRING];
+	char apellido[TAMANIO_STRING];
+	char nombreYApellido[TAMANIO_STRING_DOS];
+
 	setbuf(stdout,NULL);
+	strcpy(nombreYApellido,"");
+
+
+
+	printf("Ingrese el nombre: ");
+	__fpurge(stdin);
+	scanf("%[^\n]", nombre);
+	lenght= strlen(nombre);
+	while(lenght > TAMANIO_STRING){
+		printf("Error... Ingreso mas carecteres de los permitidos, el maximo permitido es %d\n", TAMANIO_STRING - 1);
+		printf("Ingrese el nombre: ");
+		__fpurge(stdin);
+		scanf("%[^\n]", nombre);
+		lenght= strlen(nombre);
+	}
+
+	printf("Ingrese el apellido: ");
+	__fpurge(stdin);
+	scanf("%[^\n]", apellido);
+	lenght= strlen(apellido);
+	while(lenght > TAMANIO_STRING){
+		printf("Error... Ingreso mas carecteres de los permitidos, el maximo permitido es %d\n", TAMANIO_STRING - 1);
+		printf("Ingrese el apellido: ");
+		__fpurge(stdin);
+		scanf("%[^\n]", apellido);
+		lenght= strlen(apellido);
+	}
+
+	strcat(nombreYApellido, nombre);
+	strcat(nombreYApellido, ", ");
+	strcat(nombreYApellido, apellido);
+
+	strlwr(nombreYApellido,TAMANIO_STRING_DOS);
+
+	nombreYApellido[0]=toupper(nombreYApellido[0]);
+	for(i=0;i<TAMANIO_STRING_DOS;i++){
+		if(nombreYApellido[i] == '\0'){
+			break;
+		} else {
+			if(isspace(nombreYApellido[i])){
+				nombreYApellido[i+1]=toupper(nombreYApellido[i+1]);
+			}
+		}
+	}
+	printf("Nombre y apellido: %s", nombreYApellido);
+
+
+	//printf("Nombre y apellido: %s %s", nombre, apellido);
 
 	return EXIT_SUCCESS;
+}
+
+void strlwr(char string[], int lenght){
+	int i;
+
+	for(i=0;i<lenght;i++){
+		if(string[i] == '\0'){
+			break;
+		} else {
+			string[i]=tolower(string[i]);
+		}
+	}
 }
